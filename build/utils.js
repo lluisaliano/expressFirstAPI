8309a2b1a543bf3a6f4134ff3e5fd7e3ecf02e9e
Lluís <lluisalillu@icloud.com>
2024-03-31 21:28:54 +0200

    Remove ignored files

--- build/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addActivity = void 0;
-const model_1 = require("./model");
-function addActivity(newActivity) {
-    model_1.data.push(newActivity);
-    return newActivity;
-}
-exports.addActivity = addActivity;
